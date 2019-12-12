4a9549aa68d1e68b99894a0613ac541e941dc0f5
Zoe Goldschmidt <zoe.gld@gmail.com>
2019-12-12 00:21:07 +0000

    day 1 part 2 improvement

--- day1/day1.rb
+++ day1/day1.rb
@@ -1,18 +1,19 @@
 require 'csv'
 
-lines = CSV.read('data.csv')
+lines = CSV.read('day1_data.csv')
 
 solution = []
 
 lines.each do |line|
   fuel = []
-  fuel << (line.first.to_f / 3).truncate - 2
+  fuel << (line.first.to_i / 3) - 2
   i = 0
-  while (fuel[i].to_f / 3).truncate > 2 do
-    fuel << (fuel[i].to_f / 3).truncate - 2
+  while (fuel[i] / 3) > 2 do
+    fuel << (fuel[i] / 3) - 2
     i += 1
   end
   solution << fuel.sum
 end
 
+p lines[0]
 p solution.sum
